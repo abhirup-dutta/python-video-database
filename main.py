14f74b2772de5e142146571e266ef5eb91902561
Abhirup Dutta <abhirupdutta@Abhirups-MacBook-Pro.local>
2025-08-29 14:00:04 -0400

    Safely remove previous HelloWorld framework

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from flask_restful import Api
-from src.resources.HelloWorld import HelloWorld
-from src.common.Globals import EXTENSION_HELLO_WORLD, EXTENSION_VIDEOS
+from src.common.Globals import EXTENSION_VIDEOS
 from src.resources.Video import Video
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(HelloWorld, EXTENSION_HELLO_WORLD)
+
 api.add_resource(Video, EXTENSION_VIDEOS)
 api.add_resource(Video, EXTENSION_VIDEOS + "/<int:video_id>", endpoint="/<int:video_id>")
 
--- test.py
+++ test.py
@@ -1,11 +1,7 @@
-from tests.HelloWorldTest import HelloWorldTest
+
 from tests.VideoTest import VideoTest
 
-'''
-hello_world_test = HelloWorldTest()
-hello_world_test.testGet()
-hello_world_test.testPost()
-'''
+
 
 video_test = VideoTest()
 video_test.testGet()
